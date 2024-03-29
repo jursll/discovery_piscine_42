7552059a22eb81c76e13dff7d823a94a8fe4ef45
jursll <165150282+jursll@users.noreply.github.com>
2024-03-29 12:12:57 +0100

    Delete discovery_piscine directory

--- discovery_piscine/cell03/ex00/background.js
+++
@@ -1,8 +0,0 @@
-function changeBackground() {
-    const red = Math.floor(Math.random() * 256);
-    const green = Math.floor(Math.random() * 256);
-    const blue = Math.floor(Math.random() * 256);
-
-    const randomBackground = document.querySelector("#randomBackground");
-    randomBackground.style.background = `rgb(${red}, ${green}, ${blue})`
-}
